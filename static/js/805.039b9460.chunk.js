66c4bdaa20cd3107b80066bfac3a2afe5654a8b4
DarriaGl <122396348+DarriaGl@users.noreply.github.com>
2023-08-24 17:12:39 +0000

    Deploying to gh-pages from @ DarriaGl/goit-react-hw-05-movies@efafae4845c4f8ae88d77b9e34d84c9f3ff786c4 🚀

---
+++ static/js/805.039b9460.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,o){o.r(t);var r=o(184);t.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"404 - Page Not Found"}),(0,r.jsx)("p",{children:"The page you are looking for does not exist."})]})}}}]);
+//# sourceMappingURL=805.039b9460.chunk.js.map
